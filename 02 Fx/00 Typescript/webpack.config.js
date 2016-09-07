31e74df81898118b652f06d4246bc656bfdd65ee
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2016-09-07 21:53:49 +0200

    Changed webpack.conf.js indentation to 2 spaces.

--- 02 Fx/00 Typescript/webpack.config.js
+++ 02 Fx/00 Typescript/webpack.config.js
@@ -4,40 +4,40 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var basePath = __dirname;
 
 module.exports = {
-	resolve: {
-		extensions: ['', '.js', '.ts']
-	},
-	entry: ['./students.ts'],
-	output: {
-		path: path.join(basePath, 'dist'),
-		filename: 'bundle.js'
-	},
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  entry: ['./students.ts'],
+  output: {
+    path: path.join(basePath, 'dist'),
+    filename: 'bundle.js'
+  },
 
-	devtool: 'source-map',
+  devtool: 'source-map',
 
-	module: {
-		loaders: [
-			{
-				test: /\.(ts)$/,
-				exclude: /node_modules/,
-				loader: 'ts-loader'
+  module: {
+    loaders: [
+      {
+        test: /\.(ts)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
       },
-			{
-				test: /\.js$/,
+      {
+        test: /\.js$/,
         loader: 'babel-loader',
-				exclude: /node_modules/,
+        exclude: /node_modules/,
         query: {
           presets: ['es2015']
         }
-			}
-		]
-	},
-	plugins: [
+      }
+    ]
+  },
+  plugins: [
     //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html', //Name of file in ./dist/
       template: 'index.html', //Name of template in ./src
-			hash: true
+      hash: true
     })
   ]
 }
