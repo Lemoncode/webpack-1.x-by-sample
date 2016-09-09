97b1d58b2fd91f9563831e4a2b5ae7ca7b7d4649
Braulio Diez <braulio.diez@gmail.com>
2016-09-09 22:37:34 +0200

    App in progress, issues with relative paths in webpack config, check how to solve this (ref plugin)

---
+++ 04 Misc/02 WebpackDLL/app/webpack.config.js
@@ -0,0 +1,41 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+	entry: ["./students.js"],
+	output: {
+		path: path.join(basePath, "dist"),
+		filename: "bundle.js"
+	},
+
+	devtool: 'source-map',
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+        loader: "babel-loader",
+				exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+			}
+		]
+	},
+	plugins:[
+		new webpack.DLLReferencePlugin({
+		      context: '../dll/dist',
+		      manifest: require(join(outputPath, 'jqueryStuff.json'))
+		}),
+
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+			hash: true
+    })
+  ]
+}
