e543b40becb56a0392cc544df228fc9fa981acae
Julia S.Simon <hithwen@gmail.com>
2016-10-23 16:09:58 +1100

    01-03 linting and grammar

--- 01 Styles/03 SASS/students.js
+++ 01 Styles/03 SASS/students.js
@@ -1,8 +1,10 @@
-import * as bootstrap from "./node_modules/bootstrap/dist/css/bootstrap.css"
-import * as mystyles from "./mystyles.scss"
-import {getAvg} from './averageService'
+/*jshint esversion: 6 */
 
-const scores = [90, 75, 60, 99, 94, 30]
+import * as bootstrap from "./node_modules/bootstrap/dist/css/bootstrap.css";
+import * as mystyles from "./mystyles.scss";
+import {getAvg} from './averageService';
+
+const scores = [90, 75, 60, 99, 94, 30];
 const averageScore = getAvg(scores);
 const messageToDisplay = `average score ${averageScore}`;
 
