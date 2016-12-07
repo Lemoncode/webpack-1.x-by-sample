82feb4f23eb531639b38cb6d4e82d65b342f7733
Braulio <braulio.diez@gmail.com>
2016-12-07 15:32:53 +0100

    typo id div image

--- 01 Styles/04 Handling Images/students.js
+++ 01 Styles/04 Handling Images/students.js
@@ -6,7 +6,7 @@ import logoImg from './content/logo_1.png';
 const img = document.createElement('img');
 img.src = logoImg;
 
-document.getElementById('imgContainer').appendChild(img);
+document.getElementById('imgcontainer').appendChild(img);
 
 const scores = [90, 75, 60, 99, 94, 30];
 const averageScore = getAvg(scores);
